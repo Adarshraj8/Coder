23b704fe92a8a40b5ee456f1f0c24be9ff31e633
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-01-22 21:25:57 +0530

    Time: 43 ms (6.91%), Space: 136.6 MB (26.70%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        backtrack(s,new ArrayList<>(),0,res);
+        return res;
+    }
+
+   public void backtrack(String s,List<String> l,int idx,  List<List<String>> res) 
+   {
+       if(idx==s.length())//base condition;
+       {
+           res.add(new ArrayList<>(l));
+           return;
+       }
+        String temp= "";
+       for(int i=idx;i<s.length();i++)
+       {
+           temp +=s.charAt(i);
+            if(!isPalindrome(temp))continue;
+       l.add(temp);
+            backtrack(s,l,i+1,res);
+        l.remove(l.size()-1);
+     }
+ }
+    private  boolean isPalindrome(String s)
+    {
+        int right=s.length()-1,left=0;
+        while(left<right)
+         if(s.charAt(left++)!=s.charAt(right--))
+                return false;
+            return true;
+    }
+}
