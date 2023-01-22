d5bf9c87910564b7333dfaecb0753cd49d45d86b
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-01-22 21:45:32 +0530

    Time: 17 ms (43.19%), Space: 54.7 MB (77.08%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -16,8 +16,11 @@ class Solution {
        for(int i=idx;i<s.length();i++)
        {
            temp +=s.charAt(i);
-            if(!isPalindrome(temp))continue;
+            if(!isPalindrome(temp)){
+                continue;
+            }else{
        l.add(temp);
+            }
             backtrack(s,l,i+1,res);
         l.remove(l.size()-1);
      }
@@ -25,9 +28,10 @@ class Solution {
     private  boolean isPalindrome(String s)
     {
         int right=s.length()-1,left=0;
-        while(left<right)
+        while(left<right){
          if(s.charAt(left++)!=s.charAt(right--))
                 return false;
+    }
             return true;
     }
 }
