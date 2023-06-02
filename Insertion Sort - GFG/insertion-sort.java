51f80ecbab85a461c1af32b6ad2d3d67b0e6946d
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-06-02 12:07:22 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.java
+++ Insertion Sort - GFG/insertion-sort.java
@@ -45,19 +45,18 @@ class Solution
   //Function to sort the array using insertion sort algorithm.
   public static void insertionSort(int arr[], int n)
   {
-     
-       for(int i=0;i<n-1;i++)
+     int swap=0;
+       for(int i=1;i<n;i++)
 	   {
-	      int swap=0;
-	    for(int j=0;j<n-1;j++)
-	    {
-	        if(arr[j]>arr[j+1])
-	        {
-	            swap = arr[j];
-	             arr[j]=arr[j+1];
-	             arr[j+1]=swap;
-	        }
-	    }
-	   }
+	       int key = arr[i];
+	       int j=i-1;
+	       
+	      while(j>=0 && arr[j]>key)
+	      {
+	          arr[j+1]=arr[j];
+	          j--;
+	      }
+	      arr[j+1]=key;
+	  }
   }
 }
