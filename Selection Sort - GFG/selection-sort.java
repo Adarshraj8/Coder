58647ea9737278e205010fc3f0efab70cbc6d3a2
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-10-18 19:29:21 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.java
+++ Selection Sort - GFG/selection-sort.java
@@ -36,27 +36,30 @@ class Solution
 	int  select(int arr[], int i)
 	{
         // code here such that selectionSort() sorts arr[]
-
-            return i;
+        return i;
 	}
 	
 	void selectionSort(int arr[], int n)
 	{
-	     
-	    int min_value=0;
-	     for(int i=0;i<n-1;i++)
-	     {
-	         int min_idx=i;
-	         for(int j=i+1;j<n;j++)
-	         {
-	             if(arr[j]<arr[min_idx])
-	             {
-	                 min_idx=j;
-	             }
-	         }
-	        min_value =arr[min_idx];
-	         arr[min_idx]=arr[i];
-	         arr[i] = min_value;
+	    //code here
+	    int temp[]= new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        int min_idx=0;
+	        for(int j=1;j<n;j++)
+	        {
+	            if(arr[j]<arr[min_idx])
+	            {
+	                min_idx=j;
+	            }
+	        }
+	        temp[i]=arr[min_idx];
+	        arr[min_idx]=Integer.MAX_VALUE;
+	        
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        arr[i]=temp[i];
+	    }
 	}
-}
 }
