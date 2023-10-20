c24cfc69cd8e9efa5f781014d69bb26b271e4849
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-10-20 12:09:21 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.java
+++ Bubble Sort - GFG/bubble-sort.java
@@ -17,7 +17,7 @@ class Solution
         boolean flag=false;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n-1;j++)
+            for(int j=0;j<n-1-i;j++)
             {
                 if(arr[j]>arr[j+1])
                 {
