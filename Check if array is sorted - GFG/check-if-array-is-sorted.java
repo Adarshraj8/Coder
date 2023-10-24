a0e1d1ea081da773cbf2f4c4209987a5468cc2c0
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-10-24 17:11:43 +0530

    Added solution - LeetHub

---
+++ Check if array is sorted - GFG/check-if-array-is-sorted.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+  public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            boolean ans = new Solution().arraySortedOrNot(arr, n);
+            System.out.println(ans ? 1 : 0);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+      int[] sortedArray = Arrays.copyOf(arr, n);
+        Arrays.sort(sortedArray);
+         if(Arrays.equals(arr, sortedArray))
+         return true;
+         return false;
+    }
+    }
