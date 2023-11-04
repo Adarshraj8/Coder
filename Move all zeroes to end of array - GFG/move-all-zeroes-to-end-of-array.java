290b5aab3a1736a19b5ba390319fd7ba4c4da4ab
Adarsh Raj <adarshrajgr2016@gmail.com>
2023-11-04 22:21:14 +0530

    Added solution - LeetHub

--- Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
@@ -35,26 +35,17 @@ public class Main {
 class Solution {
     void pushZerosToEnd(int[] arr, int n) {
         // code here
-          int temp[] = new int[n];
-        int k=3;
-        int idx=0;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]!=0){
-            temp[idx] =arr[i];
-            idx++;
-            }
-        }
-        for(int i=idx;i<n;i++)
-        {
-            if(arr[i]==0)
-            {
-                temp[idx]=arr[i];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=temp[i];
-        }
+        int count=0;
+       for(int i=0;i<n;i++)
+       {
+           if(arr[i]!=0)
+           {
+            
+                   int temp=arr[count];
+                   arr[count]=arr[i];
+                   arr[i]=temp;
+                    count++;
+           }
+       }
     }
 }
