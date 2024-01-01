322ca4c6e89af0a907b2aac68a4b51a8b8fe8628
Adarsh Raj <adarshrajgr2016@gmail.com>
2024-01-01 21:45:49 +0530

    Added solution - LeetHub

--- Delete a Node in Single Linked List - GFG/delete-a-node-in-single-linked-list.java
+++ Delete a Node in Single Linked List - GFG/delete-a-node-in-single-linked-list.java
@@ -88,8 +88,8 @@ class GfG
 {
     Node deleteNode(Node head, int x)
     {
-        if(head==null)
-        return null;
+       // if(head==null)
+      //  return null;
         
         if(x==1)
         return head.next;
